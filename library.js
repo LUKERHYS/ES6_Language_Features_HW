06b97eb3df98b67689004fd9edad71e0c9bd1723
lukerhys <hello@lukemelvin.com>
2020-04-19 11:30:24 +0100

    Destructuring of printInventory test passing

--- library.js
+++ library.js
@@ -22,9 +22,18 @@ Library.prototype.addBooks = function (newBooks) {
   newBooks.forEach(book => this.books.push(book));
 };
 
+// BEFORE
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+
+// AFTER
 Library.prototype.printInventory = function () {
-  this.books.forEach((book) => {
-    console.log(`${book.title} by ${book.author}`);
+  this.books.forEach(
+    {title, author} = (book) => {
+      console.log(`${title} by ${author}`);
   });
 };
 // BEFORE
